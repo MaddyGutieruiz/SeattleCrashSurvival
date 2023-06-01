c607d72747b9cf5fa48283576174fc505bd84e81
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-05-31 19:38:09 -0700

    Justins Updates

--- static/app.py
+++
@@ -1,7 +0,0 @@
-var form = document.getElementById('form');
-form.addEventListener('submit', showMessage);
-
-function showMessage(event) {
-  alert("Your response has been recorded. (Not actually, this is just a demo!)");
-  event.preventDefault();
-}
